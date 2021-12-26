d7a5e890e2e0cc7260104dd610955a71d7db4691
Brian Bycynski <high5bri@hotmail.com>
2021-12-26 08:09:27 -0500

    new project

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
 //
 //
-
-document.getElementById('name').addEventListener('blur', validateName);
-document.getElementById('zip').addEventListener('blur', validateZip);
-
-document.getElementById('email').addEventListener('blur', validateEmail);
-document.getElementById('phone').addEventListener('blur', validatePhone);
+console.log(123);
